4d9efd9b25d1eb5bfcf279d702fe9270c060989d
dbenson457 <dbenson457@hotmail.co.uk>
2024-12-21 04:45:15 +0000

    Initial Commit

---
+++ BibleJournalApp/BibleJournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleJournalAppApp.swift
+//  BibleJournalApp
+//
+//  Created by David Benson on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct BibleJournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BibleJournalAppTests/BibleJournalAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BibleJournalAppTests.swift
+//  BibleJournalAppTests
+//
+//  Created by David Benson on 21/12/2024.
+//
+
+import Testing
+@testable import BibleJournalApp
+
+struct BibleJournalAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
